b29efcbb2aea5405cac6a0330b05b1900ab35041
Liam Naylor <liam@liamnaylor.com>
2023-01-21 13:25:51 -0800

    Add short circuit logic to display 0, 1 or more spots left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,8 +17,10 @@ export default function DayListItem(props) {
 
   return (
     <li className={dayListClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
     </li>
   );
 };
