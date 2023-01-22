3e7deefe2664716841de9bbe226146422c6d9cad
Liam Naylor <liam@liamnaylor.com>
2023-01-22 11:57:25 -0800

    Refactor to remove id props to interviewerlistitem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -12,7 +12,7 @@ export default function InterviewerListItem(props) {
   };
 
   return (
-    <li className={`interviewers__item${interviewerSelected}`} onClick={() => props.setInterviewer(props.id)}>
+    <li className={`interviewers__item${interviewerSelected}`} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
