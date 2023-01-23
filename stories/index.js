f05ba7bf731ceabbde7e8da204ed824fa5f78f17
Liam Naylor <liam@liamnaylor.com>
2023-01-22 16:30:22 -0800

    Add Empty + Header components + write stories for them

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,3 +147,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time={'12pm'}/>)
+  .add("Header", () => <Header time={'12pm'}/>)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
