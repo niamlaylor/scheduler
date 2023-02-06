5d6c438be9ac245dfb1b0b933ef0adf338b24ddc
Liam Naylor <liam@liamnaylor.com>
2023-02-06 15:28:49 -0800

    Fix syntax of helpers functions

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { getAppointmentDay, getNewSpotCount, getNullAppointments } from 'helpers/selectors';
+import { getAppointmentDay, getNewSpotCount } from 'helpers/selectors';
 
 export default function useApplicationData() {
   
